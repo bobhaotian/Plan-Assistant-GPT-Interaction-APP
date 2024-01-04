b4e7152a87dc621f4ac162d8ffff43ee7b1e955d
Haotian Bao <112147566+bobhaotian@users.noreply.github.com>
2024-01-04 12:23:05 -0500

    Add files via upload

---
+++ Plan Assistant GPT Interaction APPUITests/Plan_Assistant_GPT_Interaction_APPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Plan_Assistant_GPT_Interaction_APPUITestsLaunchTests.swift
+//  Plan Assistant GPT Interaction APPUITests
+//
+//  Created by Haotian Bao on 2024-01-04.
+//
+
+import XCTest
+
+final class Plan_Assistant_GPT_Interaction_APPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
